7efa3a4ec2b9106bfce899460cf9d30f0cc4fb00
zuzustack <ridwanyamada@gmail.com>
2026-02-11 20:33:39 +0700

    initialize asp.net

--- app/Program.cs
+++ app/Program.cs
@@ -1,6 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
 var app = builder.Build();
 
-app.MapGet("/", () => Results.Ok(new { message = "Hello from ASP.NET", time = DateTime.UtcNow }));
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
